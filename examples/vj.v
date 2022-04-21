c4eb1dd3bf89659b0c14f91ec80e9b64786b9f59
R cqls <rdrouilh@gmail.com>
2022-04-21 20:52:04 +0200

    Update vj.v

--- examples/vj.v
+++ examples/vj.v
@@ -12,20 +12,19 @@ fn main() {
 	s := j.eval('"toto"').string()
 	println('s = $s')
 
-	// set a as double R vector from f64 array and check inside R
 	j.set_f64s('a', [1.1, 3, 2])
 	println(j.eval('a').f64s())
 	j.eval('println(typeof(a))')
 	j.set_f32s('a', [f32(1.1), 3, 2])
 	println(j.eval('a').f32s())
 	j.eval('println(typeof(a))')
-	// set b as integer R vector from int array and check inside R
+
 	j.set_ints('b', [1, 3, 2])
 	println(j.eval('b').ints())
-	// set c as logical R vector from bool array and check inside R
+
 	j.set_bools('c', [true, true, false])
 	println(j.eval('c').bools())
-	// set d as character R vector from string array and check inside R
+
 	j.set_strings('d', ['tutu', 'ititit'])
 	println(j.eval('d').strings())
 
